46b976aa74786b6d6bc47f07dcdd1cc0f64110b1
Zach Langer <zlanger@comverge.com>
2015-07-02 09:23:05 -0600

    added an error

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ MyFirstRailsApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'cars#index'
+  root 'car#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
